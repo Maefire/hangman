3f70ce8df4f77e936d8bcf68cb4a4e92c0662f94
MaeFire <masked_phoenix@yahoo.com>
2023-03-07 02:42:17 -0600

    Finish working game
    The game is now working from start to finish. From here, it just needs
    a save state implemented, the actual hangman printing, and other
    bells and whistles, such as coloring correct letters as green.

--- lib/game.rb
+++ lib/game.rb
@@ -6,22 +6,44 @@ class Game
 
   def initialize
     @word = random_word_generation
-    @used_letters = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+    @used_letters = []
     @guesses_left = 6
+    @guess = nil
+    # GREETING
+    game_loop
   end
 
+  def game_loop
+    # if save_exists? && (new_game != true)
+    #   load(save_number)
+    # end
+    p @word
+    loop do
+      p board_state
+      puts "Guesses remaining: #{@guesses_left}"
+      break if word_guessed?
+      print "#{@used_letters.join(" ")}\n\n\n"
+      user_guess
+      unless @word.include?(@guess) && @used_letters.include?(@guess) # letter guessed
+        @guesses_left -= 1
+      end
+    end
+  end
+
+  # #####TODO########
+  # Do we need letter available method? Could just check @used_letters.inlucde?
+  # They are the same-ish size.
+  # #####TODO########
+
   def user_guess
-    p @word # remove #
-    ##################
-    user_input = gets.chomp.downcase.delete("^a-z").to_s
-    if user_input.length == 1 && !letter_available?(user_input)
-      match_against_word(user_input)
-      @used_letters << user_input
-      word_guessed?
+    @guess = gets.chomp.downcase.delete("^a-z").to_s
+    if @guess.length == 1 && !letter_available?(@guess)
+      @used_letters << @guess
+    elsif letter_available?(@guess)
+      puts "Letter has been used. Please choose another letter."
+      user_guess
     else
       puts "Error! Please only guess 1 letter between \"a\" through \"z\""
-
-      puts "guess: #{user_input} letters: #{@used_letters}"
       user_guess
     end
   end
@@ -29,21 +51,19 @@ class Game
   def word_guessed?
     if @word.chars.all? { |char| @used_letters.include?(char) }
       puts "Congratulations! You win!"
+      true
     elsif @guesses_left == 0
       puts "You lost! The word was #{@word}"
+      true
     end
   end
 
   def to_s
-    "Word: #{@word},\n Used: #{@used_letters},\n Round: #{@guesses_left}"
+    "Word: #{@word},\n Used: #{@used_letters},\n Guess left: #{@guesses_left}"
   end
 
   def letter_available?(guess)
     @used_letters.include?(guess)
-  end
-
-  def match_against_word(guess)
-    @word.include?(guess)
   end
 
   def random_word_generation
