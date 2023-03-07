b7f5c7173a8a93b1a10b5d4387533df24fc2b984
MaeFire <masked_phoenix@yahoo.com>
2023-03-07 00:54:36 -0600

    Refactor and cleanup unused code

--- lib/game.rb
+++ lib/game.rb
@@ -6,40 +6,36 @@ class Game
 
   def initialize
     @word = random_word_generation
-    @used_letters = %w[a b c d e g h i j k l m n o p q r s t u v w x y z]
-    @win_array = []
+    @used_letters = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+    @guesses_left = 6
   end
 
-  dictionary = URI.open("dictionary.txt", "r")
-
   def user_guess
-    p @word
+    p @word # remove #
+    ##################
     user_input = gets.chomp.downcase.delete("^a-z").to_s
     if user_input.length == 1 && !letter_available?(user_input)
       match_against_word(user_input)
       @used_letters << user_input
       word_guessed?
     else
-      puts "Error! Please only guess 1 letter between a-z"
+      puts "Error! Please only guess 1 letter between \"a\" through \"z\""
 
       puts "guess: #{user_input} letters: #{@used_letters}"
       user_guess
     end
   end
 
-  ####################
-  ###   TO DO        ###
-  ### check for dupes ###
-  ###   TO DO        ###
-  ####################
   def word_guessed?
-    @used_letters.each do |letter|
-      if @word.include?(letter)
-        @win_array << letter
-      end
+    if @word.chars.all? { |char| @used_letters.include?(char) }
+      puts "Congratulations! You win!"
+    elsif @guesses_left == 0
+      puts "You lost! The word was #{@word}"
     end
-    puts @win_array.sort == @word.chars.sort
-    puts "win_array: #{@win_array},#{@win_array.length} word:#{@word},#{@word.length}"
+  end
+
+  def to_s
+    "Word: #{@word},\n Used: #{@used_letters},\n Round: #{@guesses_left}"
   end
 
   def letter_available?(guess)
@@ -54,12 +50,3 @@ class Game
     File.readlines("dictionary.txt").sample.chomp
   end
 end
-
-#################################################################################
-### Board state can be an empty array. When game starts, have the array populate.
-### The array will match the @word.length, so it will be an array of "_"s
-### When letter matches @word, then corresponding "_" will become letter
-### would not need to check for duplicates, due to how hangman works.
-### instead, just make sure each one does change. match_against_word()? could
-### be used. if true board_array[i] = user_input ?
-#################################################################################
