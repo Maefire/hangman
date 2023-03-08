5759c4cf2a24eecfe388502053eb7e1b7f3579ee
MaeFire <masked_phoenix@yahoo.com>
2023-03-08 06:36:46 -0600

    Let player choose game state
    I set it up so player can choose whether to continue a previous
    game session, or to start a new game

---
+++ lib/game.rb
@@ -0,0 +1,23 @@
+require_relative "game_logic"
+require_relative "display"
+
+class Game
+  include Display
+  include GameLogic
+
+  def play
+    puts GREETING
+    check_choice = new_or_load?
+    SinglePlayer.new.game_loop if check_choice == "1"
+    saved_game_list if check_choice == "2"
+  end
+
+  def new_or_load?
+    loop do
+      user_input = gets.chomp.downcase
+      return user_input if %w[1 2].include?(user_input)
+
+      puts "Please enter \"1\" to start a new game, or \"2\" to load a saved game"
+    end
+  end
+end
