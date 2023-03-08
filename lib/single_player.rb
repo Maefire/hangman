1bc6e593c10d109e6cc81b4f9403d155874fae36
MaeFire <masked_phoenix@yahoo.com>
2023-03-08 00:39:25 -0600

    Refactor file structure and extract game
    I decided to separate the possibilities of a game being single or multi
    player in functionality. For the future possibilities, I moved the
    code from game.rb into single_player.rb. This allows game.rb to have
    more flexibility.

---
+++ lib/single_player.rb
@@ -0,0 +1,35 @@
+require_relative "./display"
+require_relative "./game_logic"
+
+class SinglePlayer
+  include Display
+  include GameLogic
+
+  def initialize
+    @word = random_word_generation
+    @used_letters = []
+    @guesses_left = 6
+    @guess = nil
+    # GREETING
+    game_loop
+  end
+
+  def game_loop
+    # if save_exists? && (new_game != true)
+    #   load(save_number)
+    # end
+    p @word
+    loop do
+      system("clear") || system("clr")
+      puts "Guesses remaining: #{@guesses_left}"
+      puts "#{guesses_remaining(@guesses_left)}\n"
+      print "\t#{@used_letters.join(" ")}\n\n"
+      puts "\t#{board_state} \n\n"
+      break if word_guessed?
+      user_guess
+      unless @word.include?(@guess) && !letter_available?(@guess) # letter guessed
+        @guesses_left -= 1
+      end
+    end
+  end
+end
