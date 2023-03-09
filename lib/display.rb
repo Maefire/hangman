137882bc78b80fe967b87ba00663c433796d3ca3
MaeFire <masked_phoenix@yahoo.com>
2023-03-09 03:02:12 -0600

    Add small greeting with rules

--- lib/display.rb
+++ lib/display.rb
@@ -1,12 +1,21 @@
 module Display
   GREETING = <<~HEREDOC
-    welcome to Hangman. This is all sample text and place holders
+    Hangman is a guessing game for two or more players. One player thinks of a word, phrase or
+    sentence and the other(s) tries to guess it by suggesting letters within a certain number of
+    guesses. Originally a Paper-and-pencil game, there are now electronic versions.
     
-    Press the number corresponding to your option:
+    In this version of Hangman, you are put against an AI opponent. You are given only
+    6 rounds to guess the word, which will range from 5-12 letters in length. At any time during
+    the game, you may enter "save", and your game will create a time-stamped file for you.
+    \e[91m(Note: resuming a game will result in your initial save file being deleted.)\e[0m
 
-    1. New Game
 
-    2. Load Game
+      Press the number corresponding to your option:
+
+      1. New Game
+
+      2. Load Game
+    
   HEREDOC
 
   SAVEDMESSAGE = <<~HEREDOC
